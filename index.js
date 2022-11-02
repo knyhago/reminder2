ea90ba5e2ce0254400b9bc2f458622c87674b74f
knyhago <knyhago@outlook.com>
2022-11-02 22:33:08 +0530

    changed env port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose=require('mongoose');
 const cors=require('cors');
 const bodyParser=require('body-parser');
-const port=9010;
+const port=process.env.PORT||9010;
 
 const Reminder=require('./Models/reminder');
 
